930cfefd6ba1a1d35e2b5e8f8b5181293d925266
omegaui <omegaui22@gmail.com>
2023-04-08 20:28:15 +0530

    Setup Run configs!

---
+++ src/App.java
@@ -0,0 +1,15 @@
+
+import java.applet.Applet;
+import java.awt.*;
+
+/*
+    <applet code="App" width="600" height="500">
+    </applet>
+ */
+
+public class App extends Applet {
+    @Override
+    public void init() {
+
+    }
+}
